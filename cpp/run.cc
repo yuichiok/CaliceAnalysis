1c0962104217d676432eb21324cc4477b5d6d5df
yuichiok <yuichi.okugawa@cern.ch>
2022-05-03 17:45:15 +0200

    add file path

--- cpp/run.cc
+++ cpp/run.cc
@@ -7,8 +7,8 @@
 int run(TString process="default",TString filein="default",TString fileout="default"){
 
 	// filein = "../data/3.0GeV_W_run_050282/full_run.root";
-	filein = "../data/3GeV_MIPscan_eudaq_run_050490/full_run.root";
-	// filein = "../data/3GeV_MIPscan_eudaq_run_050480/full_run.root"; // *
+	// filein = "../data/3GeV_MIPscan_eudaq_run_050490/full_run.root";
+	filein = "../data/3GeV_MIPscan_eudaq_run_050480/full_run.root"; // *
 	// filein = "../data/3GeV_MIPscan_eudaq_run_050470/full_run.root";
 
 	TBEvent TBEvent(filein);
