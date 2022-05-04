ecb6503d9ef637fbce00da8b9dc9017afe5f5e5f
yuichiok <yuichi.okugawa@cern.ch>
2022-05-04 22:23:07 +0200

    update with macbook

--- cpp/run.cc
+++ cpp/run.cc
@@ -7,9 +7,11 @@
 int run(TString process="default",TString filein="default",TString fileout="default"){
 
 	// filein = "../data/3.0GeV_W_run_050282/full_run.root";
-	filein = "../data/3GeV_MIPscan_eudaq_run_050490/full_run.root";
+	// filein = "../data/3GeV_MIPscan_eudaq_run_050490/full_run.root";
 	// filein = "../data/3GeV_MIPscan_eudaq_run_050480/full_run.root"; // *
 	// filein = "../data/3GeV_MIPscan_eudaq_run_050470/full_run.root";
+
+	filein = "../../download/3GeV_MIPscan_eudaq/3GeV_MIPscan_eudaq_run_050480_build.root";
 
 	TBEvent TBEvent(filein);
 	TBEvent.AnalysisLoop();
