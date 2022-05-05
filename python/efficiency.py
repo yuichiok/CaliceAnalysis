3bf97cc5826d56bd7718c83605e970dd1a9c073c
yuichiok <yuichi.okugawa@cern.ch>
2022-05-05 12:08:42 +0200

    add eff

--- python/efficiency.py
+++ python/efficiency.py
@@ -3,4 +3,4 @@ import uproot
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = uproot.open()
+file = uproot.open("/eos/project-s/siw-ecal/TB2022-03/beamData/buildfiles/3GeV_MIPscan_eudaq/3GeV_MIPscan_eudaq_run_050470_build.root")
