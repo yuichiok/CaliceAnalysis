3ef0a34ab9be094ce60c72eb75211953745b47f9
yuichiok <yuichi.okugawa@cern.ch>
2022-05-05 16:22:22 +0200

    change uproot

--- python/efficiency.py
+++ python/efficiency.py
@@ -4,3 +4,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 file = uproot.open("/eos/project/s/siw-ecal/TB2022-03/beamData/buildfiles/3GeV_MIPscan_eudaq/3GeV_MIPscan_eudaq_run_050470_build.root")
+ecal = file['ecal']
+
