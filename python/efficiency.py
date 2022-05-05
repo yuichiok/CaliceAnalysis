f62d81fa1513023a486a316cdfb94e921fc6f00e
yuichiok <yuichi.okugawa@cern.ch>
2022-05-05 11:43:36 +0200

    add efficienc script

---
+++ python/efficiency.py
@@ -0,0 +1,6 @@
+import ROOT
+import uproot
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = uproot.open()
--- python/readfile.py
+++
@@ -1,5 +0,0 @@
-filename = "/home/calice/Work/TBData/ascii/3GeVMIPscan_run_050043/Run_Settings.txt"
-
-with open(filename) as file:
-    while line := file.readline():
-        print(line.rstrip())
