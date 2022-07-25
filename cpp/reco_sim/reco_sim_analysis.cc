f6fff43a83a7eb332b39016ad1aa9a3c94b795d5
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2022-07-25 19:06:10 +0200

    change name

--- cpp/reco_sim/reco_sim_analysis.cc
+++ cpp/reco_sim/reco_sim_analysis.cc
@@ -47,7 +47,40 @@ void slab_energy(int set_ene = 10, string particle = "e")
 	TFile * f_sim = TFile::Open(sim_file);
 	TGaxis::SetMaxDigits(3);
 
+	TH1F * rh_sum_energy;
+	TH1F * sh_sum_energy;
 
+	rh_sum_energy = (TH1F*)f_reco->Get("h_sum_energy");
+	sh_sum_energy = (TH1F*)f_sim->Get("h_sum_energy");
+
+	rh_sum_energy->Scale(1/rh_sum_energy->GetEntries());
+	sh_sum_energy->Scale(1/sh_sum_energy->GetEntries());
+
+	rh_sum_energy->GetXaxis()->SetRangeUser(0,1.5E4);
+	sh_sum_energy->GetXaxis()->SetRangeUser(0,1.5E4);
+
+	rh_sum_energy->GetYaxis()->SetRangeUser(0,0.15);
+	sh_sum_energy->GetYaxis()->SetRangeUser(0,0.15);
+
+	TCanvas *c1 = new TCanvas("c1","c1",700,700);
+	rh_sum_energy->SetLineColor(kBlue+1);
+	sh_sum_energy->SetLineColor(kBlack);
+
+	TString title = "Sum energy " + to_string(set_ene) + "GeV (reco & sim)";
+	sh_sum_energy->SetTitle(title);
+	sh_sum_energy->Draw("h");
+	rh_sum_energy->Draw("hsame");
+
+}
+
+void reco_sim_analysis()
+{
+	// const int nene = 7;
+	// int l_energy[nene] = {10, 20, 40, 60, 80, 100, 150};
+	// for (int ie=0; ie<nene; ie++){
+	// 	slab_energy(l_energy[ie]);
+	// }
+	slab_energy(10);
 
 
 }
