3fa70175264c608b0ab5762d1466e28c15ebd207
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2023-06-23 22:05:02 +0200

    store canvas

--- cpp/mask_check/macros/mask_check_analysis.cc
+++ cpp/mask_check/macros/mask_check_analysis.cc
@@ -13,6 +13,7 @@ using std::map;
 using std::vector;
 
 #define MAXV 8
+#define NSLAB 15
 
 void SetStyle()
 {
@@ -52,7 +53,7 @@ void readfiles(int energy, TFile *f_rs[])
 	f_rs[1] = TFile::Open(withmask);
 }
 
-void h_single(int set_ene, TFile *f_rs[], TString hist_name)
+void plot_h_single(int set_ene, TFile *f_rs[], TString hist_name)
 {
 	TH1F * h_nomask;
 	TH1F * h_mask;
@@ -76,28 +77,52 @@ void h_single(int set_ene, TFile *f_rs[], TString hist_name)
 
 }
 
-void h_mult(int set_ene, TFile *f_rs[], TString hist_name)
+void plot_h_2d(int set_ene, TFile *f_rs, TString hist_name)
 {
-
-	TH1F * h_nomask;
-	TH1F * h_mask;
-
-	Int_t nevents = ((TH1F*)f_rs[0]->Get("h_nhit_len"))->GetEntries();
-	h_nomask = (TH1F*)f_rs[0]->Get(hist_name);
-	h_mask   = (TH1F*)f_rs[1]->Get(hist_name);
-
-	h_nomask->Scale((Float_t)1/(Float_t)nevents);
-	h_mask->Scale((Float_t)1/(Float_t)nevents);
-
-	h_nomask->SetLineColor(kBlue+1);
-	h_mask->SetLineColor(kBlack);
+	TH2F * h2d = (TH2F*)f_rs->Get(hist_name);
 
 	TString title = hist_name + " " + TString(to_string(set_ene)) + "GeV";
-	h_nomask->SetTitle(title);
-	h_nomask->Draw("h");
-	h_mask->Draw("hsame");
+	h2d->SetTitle(title);
+	h2d->Draw("colz");
 
-	if(set_ene==10) legend(h_nomask,h_mask);
+}
+
+TCanvas * draw_h_nhit_len_slab(int set_ene, TFile *f_rs[])
+{
+	TString cname = "c_nhit_len_"+TString(to_string(set_ene))+"GeV";
+	TCanvas *c = new TCanvas(cname,cname,800,800);
+	c->Divide(4,4);
+
+	for( int islab = 0; islab<NSLAB; islab++){
+		c->cd(islab+1);
+		plot_h_single(set_ene,f_rs,"nhit_len/h_nhit_len_slab_"+TString(to_string(islab)));
+	}
+
+	return c;
+
+}
+
+vector<TCanvas*> draw_h_hit_xy(int set_ene, TFile *f_rs[])
+{
+  TString base_name[2];
+  base_name[0] = "hit_xy_"+TString(to_string(set_ene))+"GeV";
+  base_name[1] = base_name[0] + "_masked";
+
+	vector<TCanvas*> c;
+  for (int i=0; i<2; i++) {
+    c.push_back( new TCanvas("c_"+base_name[i],"c_"+base_name[i],800,800) );
+    c.at(i)->Divide(4,4);
+  }
+
+	for( int islab = 0; islab<NSLAB; islab++){
+    for (int i=0; i<2; i++) {
+      c.at(i)->cd(islab+1);
+      plot_h_2d(set_ene,f_rs[i],"hit_xy/h_hit_xy_slab_"+TString(to_string(islab)));
+    }
+		
+	}
+
+	return c;
 
 }
 
@@ -116,12 +141,8 @@ void mask_check_analysis()
 		cmap[hname]->Divide(3,3);
 	}
 
-	// TCanvas *c_hit_slab = new TCanvas("c_hit_slab","c_hit_slab",800,800);
-	// c_hit_slab->Divide(3,3);
-	// TCanvas *c_nhit_len = new TCanvas("c_nhit_len","c_nhit_len",800,800);
-	// c_nhit_len->Divide(3,3);
-
 	const int nene = 7;
+	vector<TCanvas*> c_nhit_len_slab;
 	int l_energy[nene] = {10, 20, 40, 60, 80, 100, 150};
 	for (int ie=0; ie<nene; ie++){
 		TFile *f_rs[2];
@@ -130,8 +151,10 @@ void mask_check_analysis()
 		// hit slab and nhit len
 		for( auto &hname : hnames){
 			cmap[hname]->cd(ie+1);
-			h_single(l_energy[ie],f_rs,"h_"+hname);
+			plot_h_single(l_energy[ie],f_rs,"h_"+hname);
 		}
+
+		c_nhit_len_slab.push_back(draw_h_nhit_len_slab(l_energy[ie],f_rs));
 	
 	}
 
@@ -142,6 +165,13 @@ void mask_check_analysis()
 		val->Write();
 		val->Print(outpath+val->GetName()+".png");
 	}
-	// c_hit_slab->Write();
+	TDirectory * d_nhit_len_slab = MyFile->mkdir("nhit_len_slab");
+		d_nhit_len_slab->cd();
+		for( auto ic : c_nhit_len_slab ){
+			ic->Write();
+			ic->Print(outpath+"/nhit_len_slab/"+ic->GetName()+".png");
+		}
+		MyFile->cd();
+
 
 }
